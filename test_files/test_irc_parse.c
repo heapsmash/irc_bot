f177f121b8883a5abcc95ddc740c0df43050cbab
Michael Walker <mwalk762@mtroyal.ca>
2020-05-22 00:07:28 -0600

    working state

--- test_files/test_irc_parse.c
+++ test_files/test_irc_parse.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "irc_parser.h"
+#define MAX_PARAMS 15
+#define MAX_MESSAGE 512
+
+char *g_messages[] = {
+    "PING :irc.Prison.NET",
+    ":irc.Prison.NET 004 {TEST} irc.Prison.NET ircd-ratbox-2.2.9 oiwszcerkfydnxbauglZCD biklmnopstveI bkloveI",
+    "NOTICE AUTH :*** Processing connection to irc.servercentral.net",
+    "PING :irc.servercentral.net ",
+    ":irc.servercentral.net 372 {manpage} :- -  ",
+    ":irc.servercentral.net 376 {manpage} :End of /MOTD command.",
+    ":{manpage}!~bot@d23-16-195-6.bchsia.telus.net JOIN :#c-test",
+    ":irc.servercentral.net 353 {manpage} = #c-test :{manpage} tofu1",
+    ":irc.servercentral.net 366 {manpage} #c-test :End of /NAMES list.",
+    ":tofu1!~tofu@rootstorm.com PRIVMSG #c-test :test",
+    ":tofu1!~tofu@rootstorm.com PRIVMSG {manpage} :message test",
+    ":tofu1!~tofu@rootstorm.com PRIVMSG {manpage} :PING 1590092030 937335",
+    ":tofu1!~tofu@rootstorm.com PRIVMSG {manpage} :VERSION",
+    "::dude\\!~man@fe80::2 PRIVMSG {manpage} :message test",
+    ":isTofu!~tofu@rootstorm.com PRIVMSG {manpage} :this is a test",
+    NULL,
+};
+
+typedef struct _IRC_MESSAGE_FIELDS
+{
+    char *message;
+    char *trailing;
+    char *prefix;
+    char *command;
+    char *params[MAX_PARAMS + 1];
+} IRC_MESSAGE_FIELDS;
+
+void ParseMessage(char *str, IRC_MESSAGE_FIELDS *msg);
+void ParsePrefix(char *str, IRC_MESSAGE_FIELDS *msg);
+void ParseCommand(char *str, IRC_MESSAGE_FIELDS *msg);
+void ParseParams(IRC_MESSAGE_FIELDS *msg);
+void PrintFields(IRC_MESSAGE_FIELDS msg);
+char *ChompWS(char *str);
+
+int main(int argc, char **argv)
+{
+    IRC_MESSAGE_FIELDS msg;
+    char **msg_ptr = g_messages;
+
+    while (*msg_ptr != NULL)
+    {
+        char *new_msg = strdup(*msg_ptr);
+        ParseMessage(new_msg, &msg);
+        PrintFields(msg);
+        msg_ptr++;
+    }
+
+    return 0;
+}
 
 char *ChompWS(char *str)
 {
@@ -18,23 +70,24 @@ char *ChompWS(char *str)
 
 void PrintFields(IRC_MESSAGE_FIELDS msg)
 {
-    puts("\n########################\n");
-    printf("message: \t%s\n", msg.message);
+    puts("\n########################");
+    printf("<message>: \t%s\n", msg.message);
     puts("---------------");
     if (msg.prefix != NULL)
-        printf("prefix: \t%s\n", msg.prefix);
+        printf("<prefix>: \t%s\n", msg.prefix);
     else
-        printf("prefix: NULL\n");
+        printf("<prefix>: NULL\n");
 
     puts("---------------");
-    printf("command: \t%s\n", msg.command);
-
+    printf("<command>: \t%s\n", msg.command);
     puts("---------------");
 
-    printf("params:");
+    printf("<params>:\t");
     for (int i = 0; msg.params[i] != NULL; i++)
-        printf("\t\t%s\n", msg.params[i]);
-    puts("\t\tNULL\n");
+        printf("%s,", msg.params[i]);
+    puts("NULL");
+    puts("---------------");
+    printf("<trailing>: \t%s\n", msg.trailing);
 
     puts("########################\n");
 }
@@ -90,13 +143,15 @@ void ParseParams(IRC_MESSAGE_FIELDS *msg)
         if (msg->params[i] != NULL)
         {
             char *contains_colon = strstr(msg->params[i], ":");
-            if (contains_colon)
+            if (msg->params[i][0] == ':')
             {
-                msg->params[i] = (strstr(msg->message, contains_colon)) + 1;
+                msg->params[i] = (strstr(msg->message, &msg->params[i][0])) + 1;
                 msg->params[i] = ChompWS(msg->params[i]);
+                msg->trailing = msg->params[i];
                 msg->params[i + 1] = NULL;
                 break;
             }
         }
+
     } while (msg->params[i] != NULL);
 }
